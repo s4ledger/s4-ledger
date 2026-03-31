a5eb3cf8caea3318b5f00a93f4430828ee70db98
Nick Frankfort <solusprotocol1@users.noreply.github.com>
2026-03-30 21:30:09 -0400

    Fix blank page: copy built index.html to root so Vercel serves bundled JS

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ cd prod-app && npm install && npm run build && cd ..
 cd demo-app && npm install && npm run build && cd ..
 cd S4-DemoApplication && npm install && npm run build && cd ..
 cp demo-app/dist/index.html demo-app/index.html
+cp S4-DemoApplication/dist/index.html S4-DemoApplication/index.html
 rm -f demo-app/index.monolith.html demo-app/demo.html prod-app/index.html
